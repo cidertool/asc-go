1ea6c3b6de7363f6ce1b3b5e8f99e9c60764ee7a
Renzo Robles (AWF) <renzrobles@paypal.com>
2022-04-20 12:35:52 -0500

    Adding "Manually Release an App Store Approved Version of Your App" endpoint

---
+++ asc/publishing_release_requests_test.go
@@ -0,0 +1,34 @@
+/**
+Copyright (C) 2020 Aaron Sky.
+
+This file is part of asc-go, a package for working with Apple's
+App Store Connect API.
+
+asc-go is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+asc-go is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with asc-go.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package asc
+
+import (
+	"context"
+	"testing"
+)
+
+func TestManuallyReleaseApprovedVersion(t *testing.T) {
+	t.Parallel()
+
+	testEndpointWithResponse(t, "{}", &AppStoreVersionReleaseRequestResponse{}, func(ctx context.Context, client *Client) (interface{}, *Response, error) {
+		return client.Publishing.ManuallyReleaseApprovedVersion(ctx, "")
+	})
+}
